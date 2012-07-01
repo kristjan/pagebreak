f74a26d450273d5d0e8844122dfc90d8b17c4c87
Kristján Pétursson <kristjan@gmail.com>
2012-06-30 18:35:48 -0700

    Switch to Processing.js
    Resizing and moving DOM elements is much slower than drawing to a canvas, and
    Processing handles the animation well for us.

--- pagebreak.js
+++ pagebreak.js
@@ -1,92 +1,71 @@
+var _window = $(window);
+
 Pagebreak = (function() {
-  var _game, _blocks, _ball;
-  var _window;
+  var game, ball, p;
 
   function init() {
-    _window = $(window);
-    _game = createGame();
+    game = createGame();
     startGame();
   }
 
-  function getGame() {
-    return game;
-  }
-
   function createGame() {
-    return $('<div>', {id : 'game'}).appendTo('body');
+    $('<canvas>', {
+      id : 'game',
+      width: _window.innerWidth(),
+      height: _window.innerHeight()
+    }).appendTo('body');
   }
 
   function startGame() {
-    resetBall();
-    window.animate(draw);
+    ball = new Ball();
+    p = new Processing('game', run);
   }
 
-  function resetBall() {
-    if (!_ball) _ball = createBall();
-  }
-
-  function createBall() {
-    var ball = $('<div>', {id: 'ball'}).appendTo(game);
-    return {
-      el: ball,
-      pos: {
-        top:  _game.height() - ball.width()  - 20,
-        left: _game.width()  - ball.height() - 20
-      },
-      velocity: {
-        x: -10,
-        y: -10
-      }
+  function run(p) {
+    p.frameRate(60);
+    p.size(screen.width, screen.height);
+    p.draw = function() {
+      p.background(0);
+      ball.draw(p);
     };
   }
 
-  function drawBall() {
-    _ball.el.css({
-      top: _ball.pos.top,
-      left: _ball.pos.left
-    });
-    _ball.pos.top  += _ball.velocity.y;
-    _ball.pos.left += _ball.velocity.x;
-
-    if (_ball.pos.top <= 0) _ball.velocity.y =
-      Math.abs(_ball.velocity.y);
-    if (_ball.pos.top + _ball.el.height() >= _window.innerHeight())
-      _ball.velocity.y = -Math.abs(_ball.velocity.y);
-
-    if (_ball.pos.left <= 0)
-      _ball.velocity.x = Math.abs(_ball.velocity.x);
-    if (_ball.pos.left + _ball.el.width() >= _window.innerWidth())
-      _ball.velocity.x = -Math.abs(_ball.velocity.x);
-  }
-
-  function resize() {
-    _game.css({
-      width: _window.innerWidth(),
-      height: _window.innerHeight()
-    })
-  }
-
-  function draw() {
-    drawBall();
-    window.animate(draw);
-  }
-
   return {
-    init: init,
-    getGame : getGame
+    init: init
   };
 })();
 
-$(Pagebreak.init);
+var Ball = function() {
+  this.radius   = 25;
+  this.diameter = 2 * this.radius
 
-// http://www.html5canvastutorials.com/advanced/html5-canvas-animation-stage/
-window.animate = (function(callback){
-  return window.requestAnimationFrame ||
-         window.webkitRequestAnimationFrame ||
-         window.mozRequestAnimationFrame ||
-         window.oRequestAnimationFrame ||
-         window.msRequestAnimationFrame ||
-         function(callback){
-           window.setTimeout(callback, 1000 / 60);
-         };
-})();
+  this.pos = {
+    x: _window.innerWidth()  - this.diameter - 20,
+    y: _window.innerHeight() - this.diameter - 20
+  };
+
+  this.velocity = {
+    x: -10,
+    y: -10
+  };
+
+  this.draw = function(p) {
+    p.fill(255);
+    p.ellipse(this.pos.x, this.pos.y, this.diameter, this.diameter);
+    this.update('x');
+    this.update('y');
+  };
+
+  this.update = function(coord) {
+    this.pos[coord] += this.velocity[coord];
+
+    var dimension = coord === 'x' ? 'innerWidth' : 'innerHeight';
+
+    if (this.pos[coord] <= this.radius)
+      this.velocity[coord] = Math.abs(this.velocity[coord]);
+    if (this.pos[coord] + this.radius >= _window[dimension]())
+      this.velocity[coord] = -Math.abs(this.velocity[coord]);
+  };
+}
+
+$(Pagebreak.init);
