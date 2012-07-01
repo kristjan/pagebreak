f8eff9b59928cb3952788e0976f2b0ba3fe33364
Kristján Pétursson <kristjan@gmail.com>
2012-06-30 17:05:31 -0700

    Draw gamefield and ball
    The field resizes as the window does. It's sluggish.

--- pagebreak.js
+++ pagebreak.js
@@ -1,11 +1,48 @@
 Pagebreak = (function() {
-  var game;
+  var _game, _blocks, _ball;
+  var _window;
 
   function init() {
+    _window = $(window).resize(resize);
+    _game = createGame();
+    startGame();
+  }
+
+  function getGame() {
+    return game;
+  }
+
+  function createGame() {
+    return $('<div>', {id : 'game'}).appendTo('body');
+  }
+
+  function startGame() {
+    resize();
+    resetBall();
+  }
+
+  function resetBall() {
+    if (!_ball) _ball = createBall();
+    _ball.css({
+      top:  _game.height() - _ball.width()  - 20,
+      left: _game.width()  - _ball.height() - 20
+    })
+  }
+
+  function createBall() {
+    return $('<div>', {id: 'ball'}).appendTo(game);
+  }
+
+  function resize() {
+    _game.css({
+      width: _window.innerWidth(),
+      height: _window.innerHeight()
+    })
   }
 
   return {
-    init: init
+    init: init,
+    getGame : getGame
   };
 })();
 
